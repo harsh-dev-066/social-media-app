6e5333409daa2004ce538057712429c35024b201
harsh-dev-066 <harshthakur066@gmail.com>
2024-02-18 23:46:56 +0530

    added auth routes and jwt token

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const connectDB = require("./database/db");
 const dotenv = require("dotenv");
-
-// Routes
+const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 dotenv.config();
 
+// Routes
 app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT, () => {
